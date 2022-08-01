c34c8046791ad7b781e123246f0895bd2e131518
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2022-08-01 17:49:02 -0500

    testing for server side code

--- src/utils/loadExternalScript.js
+++ src/utils/loadExternalScript.js
@@ -1,8 +1,10 @@
 const loadExternalScript = url => {
-	const script = document.createElement('script')
-	script.src = url
-	script.async = true
-	document.body.appendChild(script)
+	if (typeof document !== `undefined`) {
+		const script = document.createElement('script')
+		script.src = url
+		script.async = true
+		document.body.appendChild(script)
+	}
 }
 
 export default loadExternalScript
