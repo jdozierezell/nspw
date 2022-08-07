1240ce63b5768bdad2de046928c9bc6fd85e54b4
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2022-08-07 17:39:40 -0500

    basic background swiper in place

--- src/components/CustomGraphic/GraphicBackgroundSwiper.js
+++ src/components/CustomGraphic/GraphicBackgroundSwiper.js
@@ -1,5 +1,47 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Navigation } from 'swiper'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+
+import BackgroundClouds from '../../../static/svgs/customSVGs/BackgroundClouds.svg'
+import BackgroundGreen from '../../../static/svgs/customSVGs/BackgroundGreen.svg'
+import BackgroundRed from '../../../static/svgs/customSVGs/BackgroundRed.svg'
+import BackgroundYellow from '../../../static/svgs/customSVGs/BackgroundYellow.svg'
 
 export const GraphicBackgroundSwiper = () => {
-	return <div>GraphicBackgroundSwiper</div>
+	return (
+		<Swiper
+			pagination={{
+				clickable: true,
+			}}
+			navigation={true}
+			modules={[Pagination, Navigation]}
+			breakpoints={{
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 20,
+				},
+				1080: {
+					slidesPerView: 3,
+					spaceBetween: 30,
+				},
+			}}
+		>
+			<SwiperSlide>
+				<BackgroundClouds />
+			</SwiperSlide>
+			<SwiperSlide>
+				<BackgroundGreen />
+			</SwiperSlide>
+			<SwiperSlide>
+				<BackgroundRed />
+			</SwiperSlide>
+			<SwiperSlide>
+				<BackgroundYellow />
+			</SwiperSlide>
+		</Swiper>
+	)
 }
